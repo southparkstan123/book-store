05610395c35430c6fb926f143f119bcde2a0eaef
southparkstan123 <southparkstan123@gmail.com>
2021-03-08 17:16:13 +0900

    Add z-index as 100 and fix modal background

--- app/javascript/src/components/Modal.vue
+++ app/javascript/src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
   <FadeTransition>
-    <div class="fixed z-10 inset-0 overflow-y-auto">
+    <div class="fixed z-100 inset-0 overflow-y-auto">
       <div
         class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
       >
