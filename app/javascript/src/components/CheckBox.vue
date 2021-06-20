18ea466f4122481055263de5fe8ad8288308dcc6
southparkstan123 <southparkstan123@gmail.com>
2021-06-20 11:26:50 +0900

    Add CheckBox Component

---
+++ app/javascript/src/components/CheckBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="className">
+    <input
+      :id="inputId"
+      :type="'checkbox'"
+      :class="inputFieldClass"
+      :name="inputName"
+      @keyup="changeValue"
+      :value="inputValue"
+      :checked="isChecked"
+    />
+    <slot name="label"></slot>
+    <slot name="hints" />
+    <slot name="error-feedback" />
+  </div>
+</template>
+
+<script lang="ts">
+import inputMixin from '@/mixins/inputMixin';
+
+export default {
+  mixins: [inputMixin],
+  props: {
+    isChecked: {
+      type: Boolean,
+      default: false
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
